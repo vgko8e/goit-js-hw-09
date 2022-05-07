33810150714d4507da2613f4e003ec2929d6cd8c
vgko8e <92440969+vgko8e@users.noreply.github.com>
2022-05-08 01:57:20 +0300

    second task

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,10 +2,8 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
 const refs = {
-    input: document.querySelector('#datetime-picker'),
+    input: document.querySelectorAll('#datetime-picker'),
     startBtn: document.querySelector('button[data-start]'),
-    timer: document.querySelector('.timer'),
-    field: document.querySelector('.field'),
     days: document.querySelector('.value[data-days]'),
     hours: document.querySelector('.value[data-hours]'),
     minutes: document.querySelector('.value[data-minutes]'),
@@ -22,52 +20,70 @@ refs.startBtn.addEventListener('click', () => {
 const downTimer = {
     intervalId: null,
     isActive: false,
+    
     start() {
-        if(this.isActive) {
-            return;
-        }
+    if (this.isActive) {
+        return;
+    }
+    this.isActive = true;    
+    refs.startBtn.setAttribute('disabled', true);
 
-        this.isActive = true;
-
-        this.intervalId = setInterval(() => {
-            const currentTime = Date.now();
-            const deltaTime = refs.input.value - currentTime;
-            const { days, hours, mins, secs } = convertMs(deltaTime);
+    this.intervalId =  setInterval(() => {
+        const currentTime = Date.now();
+        const deltaTime = refs.input.value - currentTime;
+        const { days, hours, minutes, seconds } = convertMs(deltaTime);
+        updateDownTimer({ days, hours, minutes, seconds });
+        
+        console.log(`${days}:${hours}:${minutes}:${seconds}`);
         }, 1000);
     }
 }
 
-flatpickr('#datetime-picker', options);
+function updateDownTimer({ days, hours, minutes, seconds }) {
+    refs.days.textContent = days;
+    refs.hours.textContent = hours;
+    refs.minutes.textContent = minutes;
+    refs.seconds.textContent = seconds;
+};
 
 const options = {
     enableTime: true,
     time_24hr: true,
-    defaultDate: new Date(),
+    defaultDate: Date.now(),
     minuteIncrement: 1,
+
     onClose(selectedDates) {
-      console.log(selectedDates[0]);
+        refs.input.value = selectedDates[0].getTime();
+        if (refs.input.value < Date.now()) {
+            alert("Please choose a date in the future")
+        }
+        else {
+            refs.startBtn.disabled = false;
+        };
     },
 };
 
-function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
+flatpickr('#datetime-picker', options);
 
-    // Remaining days
-    const days = Math.floor(ms / day);
-    // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-  
-    return { days, hours, minutes, seconds };
+function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
 }
-  
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+function convertMs(ms) {
+// Number of milliseconds per unit of time
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+
+// Remaining days
+const days = addLeadingZero(Math.floor(ms / day));
+// Remaining hours
+const hours = addLeadingZero(Math.floor((ms % day) / hour));
+// Remaining minutes
+const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+// Remaining seconds
+const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+return { days, hours, minutes, seconds };
+}
