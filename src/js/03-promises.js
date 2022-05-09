b3be7816d471132e3cc6933ca6111d649c9b53fc
vgko8e <92440969+vgko8e@users.noreply.github.com>
2022-05-09 19:02:27 +0300

    third task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
+const formFields = document.querySelector('.form');
+formFields.addEventListener('submit', inputSubmit);
+
+function inputSubmit(evt) {
+  evt.preventDefault();
+
+  let inputFirstDelay = Number(evt.target.elements.delay.value);
+  let inputDelayStep = Number(evt.target.elements.step.value);
+  let inputAmnt = Number(evt.target.elements.amount.value);
+
+  let position = 0;
+  for (let i = 0; i < inputAmnt; i += 1) {
+    position += 1;
+
+    createPromise(position, inputFirstDelay)
+            .then(result => console.log(result))
+            .catch(error => console.log(error));
+
+    inputFirstDelay += inputDelayStep;
+  }
+  evt.currentTarget.reset();
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve (`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        // Reject
+        reject (`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    },delay)
+  })
+
 }
